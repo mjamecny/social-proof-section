e6232d0777697b5abdc8ed0ff4d1a72a6f3e80b8
mjamecny <jamecny.m@tutanota.com>
2023-11-25 18:59:29 +0100

    add colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    colors: {
+      veryDarkMagenta: "hsl(300, 43%, 22%)",
+      softPink: "hsl(333, 80%, 67%)",
+      darkGrayishMagenta: "hsl(303, 10%, 53%)",
+      lightGrayishMagenta: "hsl(300, 24%, 96%)",
+      white: "hsl(0, 0%, 100%)",
+    },
   },
   plugins: [],
 }
